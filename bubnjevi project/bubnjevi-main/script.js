06606cac2009ed5000c7cc89c296612b642d869f
Tarik Kubat <106175358+tare321@users.noreply.github.com>
2024-04-04 02:00:23 +0200

    Update script.js

--- bubnjevi project/bubnjevi-main/script.js
+++ bubnjevi project/bubnjevi-main/script.js
@@ -1,51 +1,67 @@
-window.addEventListener("keydown", (event) => {
-  let code = event.keyCode;
-  let keyElement = document.querySelector(`div[data-key="${code}"]`);
-
 let crashRide = document.querySelector('#crash-ride');
 let hiHatTop = document.querySelector('#hihat-top');
 
-const animateCrashOrRide = () => {
-  crashRide.computedStyleMap.transform = 'rotate(0deg) scale(1.5)';
+const animateCrashOrRide = () =>{
+  crashRide.style.transform = 'rotate(0deg) scale(1.5)';
 }
 
 const animateHiHatClosed = () =>{
-  hiHatTop.computedStyleMap.top = '171px';
+  hiHatTop.style.top = '171px';
 }
-/*
-window.addEventListener("keydown", (event) => {
-  let code = event.keyCode;
+
+window.addEventListener("keydown", (event) =>{                    //funkcija koja uzima sve tipke sa ekrana
+ let code = event.keyCode;
+
   let keyElement = document.querySelector(`div[data-key="${code}"]`);
 
-if(!keyElement) return;
+  if(!keyElement){
+    return;
+  }
 
-let audio = document.querySelector(`audio[data-key="${code}"]`);
-audio.currentTune = 0;
-audio.play();
+ let audio = document.querySelector(`audio[data-key="${code}"]`);            //biranje audio zvuka
+  audio.currentTime = 0;
+ audio.play();
 
-switch(Code){
-  case 69:
-  case 82:
-  animateCrashOrRide();
-  break;
-  case 75:
-    animateHiHatClosed();
-    break;
-}
+    switch(code){
+      case 69:
+      case 82:
+        animateCrashOrRide();
+        break;
+      case 73:
+      case 75:
+        animateHiHatClosed();
+        break;
+    }
+
+    keyElement.classList.add('playing');
+    
 });
-*/
-const removeCrashRideTransition = e => {
+
+//funkcije za izgled (rad sa css)
+
+const removeCrashRideTransition = e =>{
   if(e.propertyName !== 'transform') return;
 
   e.target.style.transform = 'rotate(-7.2deg) scale(1.5)';
+};
+
+const removeHiHatTopTransition = e =>{
+  if(e.propertyName !== 'top') return;
+
+ e.target.style.top = '166px';
 }
 
-const removeHiHatTopTransition = e => {
-  if(e.propertyName !== 'top') return;
+const removeKeyTransition = e => {
+  if(e.propertyName !== 'transform')return;
 
   e.target.classList.remove('playing');
 }
 
+let drumKeys = document.querySelectorAll('.key');
+drumKeys.forEach(key => {
+  key.addEventListener("transitionend", removeKeyTransition)
+})
+
+
 crashRide.addEventListener("transitionend", removeCrashRideTransition);
 hiHatTop.addEventListener("transitionend", removeHiHatTopTransition);
-
